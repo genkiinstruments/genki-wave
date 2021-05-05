a59f4fdfb01fb0a4efc752f976376f081311160a
Robert Torfason <robert.torfason@gmail.com>
2021-05-05 09:47:32 +0000

    Tmp state

--- tests/test_protocols.py
+++ tests/test_protocols.py
@@ -1,11 +1,11 @@
 import pytest
 
 from genki_wave.protocols import ProtocolAsyncio, ProtocolThread
-from tests.constants import DATA, EXPECTED
+from tests.constants import BLUETOOTH_DATA, BLUETOOH_EXPECTED, SERIAL_DATA, SERIAL_EXPECTED
 
 
 @pytest.mark.parametrize("data, expected, protocol_factory", ((DATA, EXPECTED, ProtocolThread),))
-def test_protocol_thread_serial(data, expected, protocol_factory):
+def test_protocol_thread(data, expected, protocol_factory):
     protocol = protocol_factory()
     for input_raw in data:
         protocol.data_received(input_raw)
@@ -21,13 +21,15 @@ def test_protocol_thread_serial(data, expected, protocol_factory):
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("data, expected, protocol_factory", ((DATA, EXPECTED, ProtocolAsyncio),))
-async def test_protocol_asyncio_serial(data, expected, protocol_factory):
+async def test_protocol_asyncio(data, expected, protocol_factory):
     protocol = protocol_factory()
     for input_raw in data:
         await protocol.data_received(input_raw)
 
     actual = []
-    for i in range(len(expected)):
+    # TODO(robert): This is wrong? Should not be len of expected
+    # for i in range(len(expected)):
+    while not protocol.queue.empty():
         actual.append(await protocol.queue.get())
 
     # pytest provides a much nicer output if we loop and compare each individually
--- tests/test_wave_asyncio.py
+++ tests/test_wave_asyncio.py
@@ -6,7 +6,7 @@ import pytest
 from genki_wave.callbacks import ButtonAndDataPrint
 from genki_wave.protocols import ProtocolAsyncio
 from genki_wave.asyncio import run_asyncio
-from tests.constants import BLUETOOTH_DATA, DATA
+from tests.constants import BLUETOOTH_DATA, SERIAL_DATA
 
 
 async def producer_mock(protocol, comm, data):
@@ -20,7 +20,7 @@ async def producer_mock(protocol, comm, data):
     comm.is_cancel = lambda x: True
 
 
-@pytest.mark.parametrize("protocol_factory, data", ((ProtocolAsyncio, DATA),))
-def test_run_asyncio(protocol_factory, data):
+@pytest.mark.parametrize("data", (BLUETOOTH_DATA, SERIAL_DATA))
+def test_run_asyncio(data):
     # An 'integration' test
-    run_asyncio([ButtonAndDataPrint(5)], partial(producer_mock, data=data), protocol_factory())
+    run_asyncio([ButtonAndDataPrint(5)], partial(producer_mock, data=data), ProtocolAsyncio())
