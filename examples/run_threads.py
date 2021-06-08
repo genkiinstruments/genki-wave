426afbe6a96f6d0505c83b44d83aacb6403364c8
Robert Torfason <robert.torfason@gmail.com>
2021-06-08 14:23:57 +0000

    Updated docstrings and code examples

--- examples/run_threads.py
+++ examples/run_threads.py
@@ -1,17 +1,42 @@
 import argparse
 import time
+from typing import Union
 
 from genki_wave.data.organization import ButtonEvent
 from genki_wave.discover import run_discover_bluetooth
-from genki_wave.threading import ReaderThreadSerial, ReaderThreadBluetooth
+from genki_wave.threading import ReaderThreadBluetooth, ReaderThreadSerial
 
 
-def main(reader_thread):
-    """A simple function to showcase reading from a thread. Fetches data every 0.1s"""
+class Sleeper:
+    """A simple class to sleep for an amount of time that makes sure each loop runs for `sleep_for_x_seconds` seconds"""
 
+    def __init__(self, sleep_for_x_seconds: float):
+        self._last_time = None
+        self._sleep_for_x_seconds = sleep_for_x_seconds
+
+    def sleep(self):
+        curr_time = time.time()
+        sleep_time = max(0.0, self._sleep_for_x_seconds - (self.last_time - curr_time))
+        time.sleep(sleep_time)
+        self._last_time = time.time()
+
+    @property
+    def last_time(self):
+        if self._last_time is None:
+            self._last_time = time.time()
+        return self._last_time
+
+
+def main(reader_thread: Union[ReaderThreadBluetooth, ReaderThreadSerial], fetch_data_every_x_seconds: float):
+    """A simple function to showcase reading from a thread.
+
+    Fetches data every `fetch_data_every_x_seconds` and prints out all button presses in that timespan and the
+    last value (usually a data package)
+    """
+
+    s = Sleeper(fetch_data_every_x_seconds)
     with reader_thread as wave:
-        last_time = None
-        for _ in range(50):
+        while True:
             val = wave.queue.pop_all()
             if val:
                 for v in val:
@@ -19,27 +44,22 @@ def main(reader_thread):
                         print(v)
                 print(val[-1])
 
-            curr_time = time.time()
-            if last_time is None:
-                last_time = curr_time
-            sleep_time = max(0.0, 0.1 - (last_time - curr_time))
-            time.sleep(sleep_time)
-            last_time = time.time()
+            s.sleep()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ble-address", type=str)
     parser.add_argument("--use-serial", action="store_true")
+    parser.add_argument("--fetch-data-every-x-seconds", type=float, default=1.0)
     args = parser.parse_args()
 
     if args.use_serial:
-        main(ReaderThreadSerial.from_port())
+        main(ReaderThreadSerial.from_port(), args.fetch_data_every_x_seconds)
     else:
         if args.ble_address is None:
             print("No bluetooth address (--ble-address) supplied, searching for devices...")
             run_discover_bluetooth()
         else:
-            print("Turn off by holding the `TOP` button")
-            reader_thread = ReaderThreadBluetooth.from_address(args.ble_address)
-            main(reader_thread)
+            print("Turn off using Ctrl + C (`KeyboardInterrupt`)")
+            main(ReaderThreadBluetooth.from_address(args.ble_address), args.fetch_data_every_x_seconds)
