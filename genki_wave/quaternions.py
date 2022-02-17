ba6d4b2990d40aab47e10b91f15078950cbe0ed3
Robert Torfason <robert.torfason@gmail.com>
2022-02-17 16:04:49 +0000

    Remove redundant

--- genki_wave/quaternions.py
+++ genki_wave/quaternions.py
@@ -43,27 +43,3 @@ def rotate_vector(p: Point3d, q: Quaternion) -> Point3d:
     p_rot = q * p * q.conjugate()
     return p_rot.to_point3d()
 
-
-def quat_mult(q1: Quaternion, q2: Quaternion) -> Quaternion:
-    w1, x1, y1, z1 = q1.w, q1.x, q1.y, q1.z
-    w2, x2, y2, z2 = q2.w, q2.x, q2.y, q2.z
-    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
-    x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
-    y = w1 * y2 + y1 * w2 + z1 * x2 - x1 * z2
-    z = w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2
-
-    return Quaternion(w, x, y, z)
-
-
-def quat_conj(q):
-    w, x, y, z = q
-    return w, -x, -y, -z
-#
-#
-# def rotate_vector(x: Point3d, q: Quaternion):
-#     q_conj = quat_conj(q)
-#     x_quat = [0, x[0], x[1], x[2]]
-#     out = quat_mult(quat_mult(q, x_quat), q_conj)
-#     return out[1:]
-
-
