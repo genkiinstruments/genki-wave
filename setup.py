2709c57b8b9818bc9cbc8fef16c34b0f897e7cab
Róbert Torfason <8184865+roberttorfason@users.noreply.github.com>
2022-08-09 16:50:06 +0000

    Bump minor version (#47)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requires = ["bleak", "cobs", "pyserial", "pyserial-asyncio"]
 
 setup(
     name="genki-wave",
-    version="0.3.0",
+    version="0.4.0",
     description="Python API for Wave by Genki",
     long_description=readme,
     long_description_content_type="text/markdown",
