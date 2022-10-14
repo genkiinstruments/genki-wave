ff2f200a62ddc799813822185199f6402680c35b
Robert Torfason <robert.torfason@genkiinstruments.com>
2022-10-14 18:18:09 +0000

    Fix

--- genki_wave/discover.py
+++ genki_wave/discover.py
@@ -6,8 +6,7 @@ import bleak
 async def discover_bluetooth(device_str: str):
     print("Searching for bluetooth devices...")
     devices = await bleak.BleakScanner.discover()
-    wave_devices = [d for d in devices if d.name is not None]
-    wave_devices = [d for d in wave_devices if device_str in d.name]
+    wave_devices = [d for d in devices if (d.name is not None) and (device_str in d.name)]
     print("Found the following devices" if wave_devices else "Found no devices")
     for device in wave_devices:
         print(f"Address: {device.address} - Name: {device.name}")
